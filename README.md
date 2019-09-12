52d3e15a779944820cd24546189a621e8b22edbe
AlbertUlysses <albertulysseschavez@gmail.com>
2019-09-12 16:57:28 -0700

    docs: update README.md file
    Update README.md to give a better idea of what this project folder
    consist of. Works on reverse_linked_list problem

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # leetcode-python
-Daily Data Structures and Algorithms from leetcode in the python programming language.
+Solving daily, Data Structures and Algorithms from leetcode in the Python programming language.
+
+## About
+The format of the scripts are slightly different then how other people write their solutions on GitHub. If you run my Python Script you should be able to see an output. Which means the scipt contains: the basic functions to run the script, an example of a leetcode input , a solution code, and return the solution of an example. Along with some explanation of the solution.
--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -1,4 +1,4 @@
-"""Reverses a linked list both recursively and iteratively"""
+"""Problem: Reverse a singly linked list both recursively and iteratively"""
 class Node():
     """Building blocks of a linked list"""
 
@@ -11,10 +11,12 @@ class Solution():
     
     def iter_reverse(self, head: ListNode) -> ListNode:
         """Returns a linked list reversed"""
-        tail = None
-        while head:
-            tmp = head.next
-            head.next = tail
-            tail = head
-            head = tmp
-        return tail
+        tail = None #set tail to None
+        while head: #Traverses through the linked list
+            temp = head.next #assign the temp variable to the value after the head 
+            head.next = tail #reassign the next value to the tail
+            tail = head #reassigns the tail to head 
+            head = tmp #reassigns head to the next value of the list until it's None
+        return tail #returns the new reversed linked list
+
+
