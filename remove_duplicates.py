bdd36006f335363fb5450c7f6384555e8408b99c
AlbertUlysses <albertulysseschavez@gmail.com>
2019-09-18 19:52:27 -0700

    doc: added explanation to code
    add notes to remove_duplicates on the side

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,10 +1,10 @@
 class Solution:
     """Takes in an array and returns it without duplicates, returns """
-    len_ = 1
-    if len(nums)==0:
-        return 0
-    for i in range(1,len(nums)):
-        if nums[i] != nums[i-1]:
-            nums[len_] = nums[i]
-            _len += 1
-    return _len
+    _len = 1 #set length to one
+    if len(nums)==0: # create a condition to check if the list is empty
+        return 0 # if it is empty return 0
+    for i in range(1,len(nums)): #create a for loop the length of the list
+        if nums[i] != nums[i-1]: #create a conditional to check if the nums
+            nums[len_] = nums[i] # if they match, then set it to the first place
+            _len += 1 #increment the length
+    return _len #returns the length of unique numbers in the list
--- two_sum.py
+++ two_sum.py
@@ -1,8 +1,10 @@
 class Solutions:
     def twosum(self, nums: List[int], target: int) -> List[int]:
-        dict = {}
-        for i, e in enumerate(nums):
-            if (target - nums[i]) not in dict:
-                dict[nums[i]] = i
+        dict = {} 
+        for i, e in enumerate(nums): 
+            if (target - nums[i]) not in dict:  
+                dict[nums[i]] = i 
             else:
                 return [dict[target - nums[i]], i]
+
+
