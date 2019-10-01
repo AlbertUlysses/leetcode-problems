b701f9bad68a47af68b5596ef7773c02cd2fa41c
AlbertUlysses <albertulysseschavez@gmail.com>
2019-10-01 16:09:54 -0700

    feat: a new problem
    Work in progress for problem: reverse integer

---
+++ reverse_integer.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        temp = str(x)
+        temp2 = []
+        for i, e in enumerate(temp):
+            e = int(e)
+            temp2.append(e)
+        return ans
+        for i, e in enumerate(temp2):
+            
+            expo = 1^(len(temp2)) * e
+            
