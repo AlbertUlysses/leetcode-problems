11f695f0bc1952862021fdcc0be013116e83b112
AlbertUlysses <albertulysseschavez@gmail.com>
2019-10-14 13:10:05 -0700

    feat: new solution
    New solution to reverse integer problem

--- reverse_integer.py
+++ reverse_integer.py
@@ -1,12 +1,20 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        temp = str(x)
-        temp2 = []
-        for i, e in enumerate(temp):
-            e = int(e)
-            temp2.append(e)
+        
+        temp = x
+        if x < 0:
+            temp = x * -1
+        str_ = str(temp)
+        ans = ''
+        for e in str_:
+            ans = e + ans
+            str_ = str_[:-2]
+        ans = int(ans)
+        if x < 0:
+            ans = -1 * ans
+        if (ans < (-2**31)) or (ans > (2**31)-1):
+            return 0 
         return ans
-        for i, e in enumerate(temp2):
-            
-            expo = 1^(len(temp2)) * e
-            
+
+"""Runtime: 40 ms, faster than 60.07% of Python3 online submissions for Reverse Integer.
+Memory Usage: 14.1 MB, less than 5.26% of Python3 online submissions for Reverse Integer."""
