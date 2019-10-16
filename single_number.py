955cfd3ab7666b77f2c6d165d2ebfcf1444c7455
AlbertUlysses <albertulysseschavez@gmail.com>
2019-10-16 15:11:24 -0700

    feat: update code
    Entered different code that I enjoyed more

--- single_number.py
+++ single_number.py
@@ -1,9 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans = 0
-        for e in nums:
-            ans = ans^e
-        return ans
-
+        return 2 * sum(set(nums) - sum(nums))
+     
+    
 """Runtime: 96 ms, faster than 88.85% of Python3 online submissions for Single Number.
-Memory Usage: 16.4 MB, less than 6.56% of Python3 online submissions for Single Number."""
+Memory Usage: 16.3 MB, less than 6.56% of Python3 online submissions for Single Number."""
