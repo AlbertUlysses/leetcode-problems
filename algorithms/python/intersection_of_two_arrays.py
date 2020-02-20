84bc2032a671f0c90425557f0af8ee8c8f546457
AlbertUlysses <albertulysseschavez@gmail.com>
2020-02-19 20:01:59 -0800

    fix: have intersection of two arrays running

--- algorithms/python/intersection_of_two_arrays.py
+++ algorithms/python/intersection_of_two_arrays.py
@@ -1,14 +1,14 @@
-# This code is wrong, work in progress
+# The solution runs at O(nlogn) it can be improved 
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         _list = []
+        nums1 = sorted(nums1)
+        nums2 = sorted(nums2)
         for i in nums2:
-            if i in _list:
-                pass
-            else:
+            if i not in _list:
                 low = 0
                 high = len(nums1) - 1
-                while low <= high:
+                while low <= high and i not in _list:
                     mid = (low + high) // 2
                     if i == nums1[mid]:
                         _list.append(i)
@@ -17,3 +17,7 @@ class Solution:
                     else:
                         low = mid + 1
         return _list
+"""
+Runtime: 84 ms, faster than 6.90% of Python3 online submissions for Intersection of Two Arrays.
+Memory Usage: 12.6 MB, less than 100.00% of Python3 online submissions for Intersection of Two Arrays.
+"""
