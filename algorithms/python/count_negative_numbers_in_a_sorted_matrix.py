8c184eab3f98e10d766d3f3bbfaf86ee0caabe0f
AlbertUlysses <albertulysseschavez@gmail.com>
2020-02-21 20:03:04 -0800

    feat: new problem

---
+++ algorithms/python/count_negative_numbers_in_a_sorted_matrix.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        # TODO: write for-loop for each row in matrix
+        # TODO: implement binary search on each list that finds the point where it is greater than 0
+        # target here is always 0, as oppossed to normal binary search
+        # return the remainder leng
+        low = 0
+        high
