f03805260b7e1f69a92d341ea26066633e0e7498
AlbertUlysses <albertulysseschavez@gmail.com>
2020-02-26 19:27:59 -0800

    fix: Count Negative runs

--- algorithms/python/count_negative_numbers_in_a_sorted_matrix.py
+++ algorithms/python/count_negative_numbers_in_a_sorted_matrix.py
@@ -3,30 +3,26 @@ class Solution:
         neg_count = 0
         for row in range(len(grid)):
             list_ = grid[row]
-            count = 0
+            count = None 
             if list_[0] < 0:
                 neg_count += len(list_)
-                count += 1
+                count = 1
+            if list_[-1] >= 0:
+                count = 1
             low = 0
             high = len(list_) - 1
-            while low <= high and count == 0:
+            while low <= high and count is None:
                 mid = (low + high) // 2
                 if list_[mid + 1] < 0 and list_[mid] >= 0:
                     neg_count +=  len(list_[mid+1:])
-                    count += 1
+                    count = 1
                 elif list_[mid] >= 0:
                     low = mid + 1
-                else:
+                elif list_[mid] < 0:
                     high =  mid - 1
-        return count 
+        return neg_count 
 
-
-list_ = [[8,-2,-2,-2,-4,-5,-5],[-2,-3,-3,-4,-4,-5,-5],[-2,-5,-5,-5,-5,-5,-5],[-3,-5,-5,-5,-5,-5,-5],[-5,-5,-5,-5,-5,-5,-5],[-5,-5,-5,-5,-5,-5,-5],[-5,-5,-5,-5,-5,-5,-5],[-5,-5,-5,-5,-5,-5,-5],[-5,-5,-5,-5,-5,-5,-5]]
-
-list_2 = [5, 4, 3]
-
-test = Solution()
-test2 = Solution()
-
-print(test.countNegatives(list_))
-#print(test.count_neg(list_2))
+"""
+Runtime: 124 ms, faster than 81.93% of Python3 online submissions for Count Negative Numbers in a Sorted Matrix.
+Memory Usage: 13.8 MB, less than 100.00% of Python3 online submissions for Count Negative Numbers in a Sorted Matrix.
+"""
