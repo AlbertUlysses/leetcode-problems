e48bf8825dd1b67dcd41412447ac283c5e54dae6
AlbertUlysses <albertulysseschavez@gmail.com>
2020-04-15 09:51:57 -0700

    feat: update

--- algorithms/python/single_number.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        return 2 * sum(set(nums) - sum(nums))
-     
-    
-"""Runtime: 96 ms, faster than 88.85% of Python3 online submissions for Single Number.
-Memory Usage: 16.3 MB, less than 6.56% of Python3 online submissions for Single Number."""
